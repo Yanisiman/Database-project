1912a56d83817cce52c0845e45213e15f61c6255
yanis chaabane <yanis.chaabane@epita.fr>
2020-04-11 15:54:49 +0200

    Hardcode the database because this is the one we must work with

--- src/gui/ApplicationLauncher.java
+++ src/gui/ApplicationLauncher.java
@@ -1,10 +1,16 @@
 package gui;
 
+import businessLogic.BusinessFacade;
+
 public class ApplicationLauncher {
 	public static void main(String[] args) {
 		
-		ConnectDatabaseGUI connectDatabaseGUI = new ConnectDatabaseGUI();
-		connectDatabaseGUI.setVisible(true);
+		/*ConnectDatabaseGUI connectDatabaseGUI = new ConnectDatabaseGUI();
+		connectDatabaseGUI.setVisible(true);*/
+		
+		BusinessFacade bl = new BusinessFacade("DBDI05", "zwV4Y9Mk");
+		LoginGUI loginGUI = new LoginGUI(bl);
+		loginGUI.setVisible(true);
 		
 	}
 }
